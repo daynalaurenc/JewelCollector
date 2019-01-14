070373a2c0911aa1fc76068154eca6b572efef9c
daynalaurenc <daynalaurenc@gmail.com>
2019-01-13 20:24:06 -0800

    figure out score counter/css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,115 +3,101 @@ $(document).ready(function() {
 var randomNumber = "";
 var win = 0;
 var loss = 0;
-var totalNumber = "";
 var jewels = [];
+var totalNumber = 0;
 var redJewel = "";
 var blueJewel = "";
 var yellowJewel = "";
 var greenJewel = "";
+    
+var computerChoice = 19 + Math.floor(Math.random() * 120);
+$("#random-number").text(computerChoice);
 
 function resetGame() {
-    var computerChoice = 19 + Math.floor(Math.random() * 120);
+    computerChoice;
+    totalNumber = 0;
     for(var i = 0; i < 4; i++) {
         var jewelRandom = 1 + Math.floor(Math.random() * 12);
-        var jewels = $("#jewels");
-        $("#jewels").append(jewels);
+        jewels.push(jewelRandom);
+         
+
+        // var jewel = $("<div>");
+        //     jewel.attr({
+        //         "class": "jewel",
+        //         "data-random": jewelRandom,
+        //     });
+        // $(".jewels").append(jewel);
         console.log(jewelRandom);
+            
     };
-
-    redJewel = [0];
-    blueJewel = [1];
-    yellowJewel = [2];
-    greenJewel = [3];
-
+    
+    
+    redJewel = jewels[0];
+    blueJewel = jewels[1];
+    yellowJewel = jewels[2];
+    greenJewel = jewels[3];
+    
     $("#random-number").text(computerChoice);
     // $("#random-number").html("Random Number: " + computerChoice);
-    $("#total-number").text(totalNumber) 
-
+    $("#finalNumber").text(totalNumber) 
+    
 };
-
+    
 resetGame();
+   
+// $("#red").on("click", function() {
+    
+//     var num = parseInt($(this).attr("data-random"));
+    
+//     totalNumber += num;
+
+//     console.log(totalNumber);
 
 
 $("#red").on("click", function() {
-    var totalNumber = (totalNumber + redJewel);
+    totalNumber = (totalNumber + redJewel);
+    $("#finalNumber").text(totalNumber);
     statement();
     console.log("Red Jewel is clicked!", totalNumber);
 });
-
+        
 $("#blue").on("click", function() {
-    var totalNumber = (totalNumber + blueJewel);
+    totalNumber = (totalNumber + blueJewel);
+    $("#finalNumber").text(totalNumber);
     statement();
     console.log("Blue Jewel is clicked!", totalNumber);
 });
-
+        
 $("#yellow").on("click", function() {
-    var totalNumber = (totalNumber + yellowJewel);
+    totalNumber = (totalNumber + yellowJewel);
+    $("#finalNumber").text(totalNumber);
     statement();
     console.log("Yellow Jewel is clicked!", totalNumber);
 });
-
+        
 $("#green").on("click", function() {
-    var totalNumber = (totalNumber + greenJewel);
+    totalNumber = (totalNumber + greenJewel);
+    $("#finalNumber").text(totalNumber);
     statement();
     console.log("Green Jewel is clicked!", totalNumber);
-    // var num = $(this).val();
+
 });
 
 function statement() {
-    if (totalNumber === randomNumber) {
+    if (totalNumber === computerChoice) {
         win++;
         $("#wins").text("Wins: " + win);
         resetGame();
     }
-
-    else if (totalNumber > randomNumber) {
+    
+    else if (totalNumber > computerChoice) {
         losses++;
         $("#losses").text("Losses: " + losses);
         resetGame();
     }
-
+    
+    
 };
 
 });
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-// switch(jewels) {
-//     case "var redJewel":
-//     result = redJewel + redJewel;
-//     result = redJewel + blueJewel;
-//     result = redJewel + yellowJewel;
-//     result = redJewel + greenJewel;
-//     break;
-
-//     case "var blueJewel":
-//     result = blueJewel + blueJewel;
-//     result = blueJewel + yellowJewel;
-//     result = blueJewel + greenJewel;
-//     break;
-
-//     case "var yellowJewel":
-//     result = yellowJewel + yellowJewel;
-//     result = yellowJewel + greenJewel;
-//     break;
-
-//     case "var greenJewel":
-//     result = greenJewel + greenJewel
-//     break;
-
-
-
 
